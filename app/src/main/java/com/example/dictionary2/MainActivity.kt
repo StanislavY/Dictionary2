445e9329c8f6d18e2e355fe10f5de4835453aa0d
s.yashenko <travikubani@mail.ru>
2021-11-30 18:24:12 +0300

    initial commit

---
+++ app/src/main/java/com/example/dictionary2/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.dictionary2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.dictionary2.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
